aac13c90c493a26b548dc4d63623092fd1b4cfe4
gelaws-hub <fadhilgelaws@gmail.com>
2025-06-10 14:07:54 +0700

    added new cors domain config for the web frontend hosting

--- PokerGameCore/Program.cs
+++ PokerGameCore/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
         policy
-            .WithOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:5173", "https://gourav-d.github.io")
+            .WithOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:5173", "https://gourav-d.github.io", "https://pokerminuman.web.app")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
